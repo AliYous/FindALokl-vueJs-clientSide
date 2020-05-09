2def69ab3bc2b6ad783789d80435ef7df3d5dcbe
ali <aliyoussef34@hotmail.fr>
2020-05-09 03:57:20 +0200

    split the Conversations view in multiple components

--- src/components/messages/ConversationsList.vue
+++ src/components/messages/ConversationsList.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-        
+    <div class="conversations-list">
+        <v-card outlined class="conversation-item">
+            <v-card-title>Adrien Herry</v-card-title>
+            <v-card-subtitle>Dernier message de la conversation</v-card-subtitle>
+        </v-card>
+        <v-card outlined  class="conversation-item">
+            <v-card-title>Adrien Herry</v-card-title>
+            <v-card-subtitle>Dernier message de la conversation</v-card-subtitle>
+        </v-card>
+        <v-card outlined  class="conversation-item">
+            <v-card-title>Adrien Herry</v-card-title>
+            <v-card-subtitle>Dernier message de la conversation</v-card-subtitle>
+        </v-card>
+        <v-card outlined  class="conversation-item">
+            <v-card-title>Adrien Herry</v-card-title>
+            <v-card-subtitle>Dernier message de la conversation</v-card-subtitle>
+        </v-card>
     </div>
 </template>
 
@@ -11,6 +26,22 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+ .conversations-list {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        height: 100%;
+        /* background: #F8F8F8 none repeat scroll 0 0; */
+        width: 27rem;
+        overflow-y: scroll;
+    }
+    .conversation-item {
+        background: #F8F8F8 none repeat scroll 0 0;
+    }
+    .conversation-item:hover {
+        background: #EBEBEB none repeat scroll 0 0;
+        cursor: pointer;
+    }
 
 </style>
--- src/components/messages/MessagesFeed.vue
+++ src/components/messages/MessagesFeed.vue
@@ -1,7 +1,31 @@
 <template>
-    <div>
-        
-    </div>
+    <div class="messages-container container">
+                <div class="messages-feed container">
+                    <div class="user-sent-message message-row">
+                        <v-card class="message sent">
+                            <p>this is a message that is supeeer long Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam blanditiis at voluptatum ipsam qui voluptates iste rerum accusamus iure recusandae.</p>
+                        </v-card>
+                    </div>
+                    <div class="user-received-message message-row">
+                        <v-card class="message received">
+                            <p>this is a message</p>
+                        </v-card>
+                    </div>
+                    <div class="user-sent-message message-row">
+                        <v-card class="message sent">
+                            <p>this is a message</p>
+                        </v-card>
+                    </div>   
+                </div>
+                <div class="send-message-field">
+                    <v-text-field
+                    class="text-field"
+                    label="Message ..."
+                    auto-grow
+                    ></v-text-field>
+                    <v-icon class="send-icon">mdi-send</v-icon>
+                </div>
+            </div>
 </template>
 
 <script>
@@ -11,6 +35,64 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .messages-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        /* background-color: red; */
+        height: 100%;
+        width: 49rem;
+    }
+    .messages-feed {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        /* background-color: greenyellow; */
+        height: 85%;
+        margin-bottom: 1rem;
+        overflow-y: scroll;
+    }
+    .message-row {
+        width: 100%;
+    }
+    .message {
+        width: 70%;
+        margin-top: 0.5rem;
+        padding-right: 0.6rem;
+        padding-left: 0.6rem;
+        padding-top: 0.3rem;
+    }
+    .user-sent-message {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+    }
+    .sent {
+        background: #05728f none repeat scroll 0 0;
+        color: white;
+    }
+    .user-received-message {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+    }
+    .received {
+        background: #ebebeb none repeat scroll 0 0;
+    }
+    .send-message-field {
+        /* background-color: green; */
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        width: 100%;
+        height: 15%
+    }
+    .text-field {
+        margin-right: 1rem;
+    }
+    .send-icon:hover {
+        color: #F38633;
+        cursor: pointer;
+    }
 </style>
