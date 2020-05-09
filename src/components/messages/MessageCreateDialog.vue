c594513a7ae0e7aae1e3c4ec6ad87e7523f16007
ali <aliyoussef34@hotmail.fr>
2020-05-09 04:24:01 +0200

    modified the flow of the MessageCreateDialog

--- src/components/messages/MessageCreateDialog.vue
+++ src/components/messages/MessageCreateDialog.vue
@@ -1,46 +1,62 @@
 <template>
-  <v-row justify="center">
-    <v-btn
-      color="#F38633"
-      dark
-      :block="btnBlock"
-      style="margin-left: 2rem"
-      @click.stop="dialog = true"
-    >
-      Contact {{localName}}
-    </v-btn>
+  <div>
+    <v-row justify="center">
+      <v-btn
+        color="#F38633"
+        dark
+        :block="btnBlock"
+        style="margin-left: 2rem"
+        @click.stop="dialog = true"
+      >
+        Contact {{localName}}
+      </v-btn>
 
-    <v-dialog
-      v-model="dialog"
-      max-width="800"
-    >
-      <v-card>
-        <v-card-title>Send a message to {{localName}} </v-card-title>
-        <v-card-subtitle>Tell him/her when you plan on visiting the city, what are some of the activities you want to do and ask any question you want ! </v-card-subtitle>
-
-
-        <v-textarea
-          class="mx-2"
-          label="Your Message"
-          rows="6"
-          prepend-icon="mdi-comment"
-        ></v-textarea>
+      <v-dialog
+        v-model="dialog"
+        max-width="800"
+      >
+        <v-card>
+          <v-card-title>Send a message to {{localName}} </v-card-title>
+          <v-card-subtitle>Tell him/her when you plan on visiting the city, what are some of the activities you want to do and ask any question you want ! </v-card-subtitle>
 
 
+          <v-textarea
+            class="mx-2"
+            label="Your Message"
+            rows="6"
+            prepend-icon="mdi-comment"
+          ></v-textarea>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="green darken-1"
-            text
-            @click="dialog = false"
-          >
-            Send Message
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-row>
+
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="green darken-1"
+              text
+              @click.prevent="sendMessage()"
+            >
+              Send
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+
+    <!-- Message sent confirmation -->
+    <v-snackbar
+        v-model="snackbar"
+      >
+        Message was successfully sent to {{localName}}!
+        <v-btn
+          color="pink"
+          text
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </v-snackbar>
+    </div>
 </template>
 
 
@@ -55,12 +71,19 @@ export default {
             local_id: '',
             message: '',
             loading: false,
-            error: null
+            error: null,
+            snackbar: false
         }
     },
     methods: {
         closeDialog() {
             this.dialog = false;
+        },
+        sendMessage() {
+          this.loading = true;
+          this.dialog = false;
+          this.snackbar = true;
+          this.loading = false;
         }
     }
 
